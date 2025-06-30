58872c1639711c7a7596e8ff2755554bc9b50a64
d3tris <daurov97@gmail.com>
2025-06-30 14:29:41 +0200

    Automated testing practice

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -3,9 +3,9 @@ def test_user_login():
 
 class TestUserLogin:
     def test_1(self):
-        print("Hello!")
+        ...
     def test_2(self):
-        print("Hello!")
+        ...
 
 def test_assert_positive_case():
     assert(2 + 2) == 4
