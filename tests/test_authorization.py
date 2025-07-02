c620df6716b086c10c6210e410e49722663aefc2
d3tris <daurov97@gmail.com>
2025-07-02 22:35:47 +0200

    Practice with Pytest parameterization

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,25 +1,34 @@
-from playwright.sync_api import expect, Page
+from playwright.sync_api import expect, sync_playwright  # Page
 import pytest
 
 
 @pytest.mark.regression
 @pytest.mark.authorization
-def test_wrong_email_or_password_authorization(chromium_page: Page):
-    # with sync_playwright() as playwright:
-    #     browser = playwright.chromium.launch(headless=False)
-    #     page = browser.new_page()
+@pytest.mark.parametrize(
+    "email, password",
+    [
+        ("user.name@gmail.com", "password"),
+        ("user.name@gmail.com", "  "),
+        ("  ", "password")
+    ]
+)
+# def test_wrong_email_or_password_authorization(chromium_page: Page):
+def test_wrong_email_or_password_authorization(email: str, password: str):
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        page = browser.new_page()
 
-    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
-    email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
-    email_input.fill('user.name@gmail.com')
+        email_input = page.get_by_test_id('login-form-email-input').locator('input')
+        email_input.fill(email)
 
-    password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
-    password_input.fill('Password')
+        password_input = page.get_by_test_id('login-form-password-input').locator('input')
+        password_input.fill(password)
 
-    login_button = chromium_page.get_by_test_id('login-page-login-button')
-    login_button.click()
+        login_button = page.get_by_test_id('login-page-login-button')
+        login_button.click()
 
-    wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
-    expect(wrong_email_or_password_alert).to_be_visible()
-    expect(wrong_email_or_password_alert).to_have_text('Wrong email or password')
+        wrong_email_or_password_alert = page.get_by_test_id('login-page-wrong-email-or-password-alert')
+        expect(wrong_email_or_password_alert).to_be_visible()
+        expect(wrong_email_or_password_alert).to_have_text('Wrong email or password')
--- tests/test_parametrization.py
+++ tests/test_parametrization.py
@@ -9,13 +9,13 @@ def test_numbers(number: int):
 
 @pytest.mark.parametrize('number, expected', [(1, 1), (2, 4), (3, 9)])
 def test_several_numbers(number: int, expected: int):
-    assert number ** 2 == expected
+    ...#assert number ** 2 == expected
 
 
 @pytest.mark.parametrize('os', ['macos', 'windows', 'linux', 'debian'])
 @pytest.mark.parametrize('browser', ['chromium', 'webkit', 'firefox'])
 def test_multiplication_of_numbers(os: str, browser: str):
-    assert len(os + browser) > 0
+    ...#assert len(os + browser) > 0
 
 
 @pytest.fixture(params=['chromium', 'webkit', 'firefox'])
