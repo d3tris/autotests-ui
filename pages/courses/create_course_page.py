ecf194f06f42888cac304f8d0baf219cb7b26e9c
d3tris <daurov97@gmail.com>
2025-07-14 20:31:17 +0200

    Test base growth and refactoring

--- pages/courses/create_course_page.py
+++ pages/courses/create_course_page.py
@@ -3,6 +3,7 @@ from components.courses.create_course_exercises_toolbar_view_component import Cr
 from components.courses.create_course_form_component import CreateCourseFormComponent
 from components.courses.create_course_toolbar_view_component import CreateCourseToolbarViewComponent
 from components.navigation.navbar_component import NavbarComponent
+from components.navigation.sidebar_component import SidebarComponent
 from components.views.empty_view_component import EmptyViewComponent
 from components.views.image_upload_widget_component import ImageUploadWidgetComponent
 from pages.base_page import BasePage
@@ -14,8 +15,9 @@ class CreateCoursePage(BasePage):
         super().__init__(page)
 
         self.navbar = NavbarComponent(page)
-        self.create_course_form = CreateCourseFormComponent(page)
+        self.sidebar = SidebarComponent(page)
         self.toolbar_view = CreateCourseToolbarViewComponent(page)
+        self.create_course_form = CreateCourseFormComponent(page)
         self.exercises_toolbar_view = CreateCourseExercisesToolbarViewComponent(page)
         self.create_course_exercise_form = CreateCourseExerciseFormComponent(page)
         self.image_upload_widget = ImageUploadWidgetComponent(page, 'create-course-preview')
--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -17,9 +17,7 @@ class TestCourses:
         create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
         create_course_page.toolbar_view.check_visible()
         create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
-        create_course_page.create_course_form.check_visible(
-            title='', estimated_time='', description='', max_score='0', min_score='0'
-        )
+        create_course_page.create_course_form.check_visible(title='', estimated_time='', description='', max_score='0', min_score='0')
         create_course_page.exercises_toolbar_view.check_visible()
         create_course_page.check_visible_exercises_empty_view()
 
@@ -37,4 +35,43 @@ class TestCourses:
         courses_list_page.toolbar_view.check_visible()
         courses_list_page.course_view.check_visible(
             index=0, title='Playwright', max_score='100', min_score='10', estimated_time='2 weeks'
+        )
+
+    def test_edit_course(self, create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
+        create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+        create_course_page.toolbar_view.check_visible()
+        create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
+        create_course_page.create_course_form.check_visible(title='', estimated_time='', description='', max_score='0', min_score='0')
+        create_course_page.exercises_toolbar_view.check_visible()
+        create_course_page.check_visible_exercises_empty_view()
+
+        create_course_page.create_course_form.fill(
+            title='Practice',
+            estimated_time='1 Month',
+            description='Practice with Page Object, Page Component and Page Factory',
+            max_score='1000',
+            min_score='950'
+        )
+        create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+        create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
+        create_course_page.toolbar_view.click_create_course_button()
+
+        courses_list_page.toolbar_view.check_visible()
+        courses_list_page.course_view.check_visible(
+            index=0, title='Practice', max_score='1000', min_score='950', estimated_time='1 Month'
+        )
+
+        courses_list_page.course_view_menu.click_edit(index=0)
+        create_course_page.create_course_form.fill(
+            title='Practice Update',
+            estimated_time='2 Months',
+            description='Practice with refactoring',
+            max_score='1500',
+            min_score='1400'
+        )
+        create_course_page.toolbar_view.click_create_course_button()
+
+        courses_list_page.toolbar_view.check_visible()
+        courses_list_page.course_view.check_visible(
+            index=0, title='Practice Update', max_score='1500', min_score='1400', estimated_time='2 Months'
         )
