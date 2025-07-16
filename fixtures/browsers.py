9db6a36203f698a2bf30704d3e6be82d9682bc57
d3tris <daurov97@gmail.com>
2025-07-16 22:00:49 +0200

    Autotest settings with Pydantic

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,9 +1,12 @@
 import pytest
 from _pytest.fixtures import SubRequest
 from playwright.sync_api import Page, Playwright
+
 from pages.dashboard.dashboard_page import DashboardPage
 from pages.authentication.registration_page import RegistrationPage
 from tools.playwright.pages import initialize_playwright_page
+from config import settings
+
 
 @pytest.fixture
 def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
@@ -19,14 +22,22 @@ def initialize_browser_state(playwright: Playwright):
 
     registration_page = RegistrationPage(page=page)
     registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-    registration_page.registration_form.fill(email="user.name@gmail.com", username="username", password="password")
+    registration_page.registration_form.fill(
+        email=settings.test_user.email,
+        username=settings.test_user.username,
+        password=settings.test_user.password
+    )
     registration_page.click_registration_button()
     # registration_page.page.wait_for_timeout(2000)
     dashboard_page.dashboard.check_visible()
-    context.storage_state(path="browser-state.json")
+    context.storage_state(path=settings.browser_state_file)
     browser.close()
 
 
 @pytest.fixture
 def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright) -> Page:
-    yield from initialize_playwright_page(playwright, test_name=request.node.name, storage_state="browser-state.json")
+    yield from initialize_playwright_page(
+        playwright,
+        test_name=request.node.name,
+        storage_state=settings.browser_state_file
+    )
